c76cb88d910166b8fea95cff15e9e5091bba7d08
Linnchoeuh <lennintendo@gmail.com>
2021-04-11 20:53:11 +0200

    a

--- main.js
+++ main.js
@@ -142,7 +142,7 @@ document.addEventListener("webkitfullscreenchange", function () {
 import {Tool_Kit, Timer_Log} from "./includes/tools.js";
 // import {Pause} from "./includes/pause.js";
 // import {Transition} from "./includes/transition.js";
-// import {Canvas_resolution_asset} from "./includes/fullscreen_asset.js"
+import {Canvas_resolution_asset} from "./includes/fullscreen_asset.js"
 // import {FPS} from "./includes/display/fps_cap.js"
 // import {Animatic} from "./includes/animatic.js";
 // import {Map_Data} from "./includes/level_reader.js";
