26f5cd9d77fc780bcba4b78f608586c1103db6cb
Linnchoeuh <lennintendo@gmail.com>
2021-04-12 03:44:48 +0200

    q

--- includes/gui/test.js
+++ includes/gui/test.js
@@ -2,7 +2,18 @@ import {Tools, MapData} from "../../main.js";
 
 class Une_Classe
 {
+    constructor(ctx)
+    {
+        this.ctx = ctx
+        this.grd = ctx.createLinearGradient(-150, 0, Tools.resolutionScaler(3000), 0);
+        this.grd.addColorStop(0.1, "transparent");
+        this.grd.addColorStop(0, "black");
 
+        this.pauseframe = 0;
+        this.pause = false;
+        this.pkey = false;
+        this.endpause = false;
+    }
 }
 
 export{Une_Classe};
