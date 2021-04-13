556a1606ab79762e3cabcae678ff0be4b2058645
Linnchoeuh <lennintendo@gmail.com>
2021-04-13 16:35:48 +0200

    a

--- main.js
+++ main.js
@@ -210,8 +210,8 @@ export{Fullscreen};
 import{Main_Menu} from "./includes/menus/main_menu/main_menu.js";
 const MainMenu = new Main_Menu();
 
-import{Game_Menu} from "./includes/menus/game_menu/game_menu.js";
-const GameMenu = new Game_Menu();
+// import{Game_Menu} from "./includes/menus/game_menu/game_menu.js";
+// const GameMenu = new Game_Menu();
 
 // import{Setting_Menu} from "./includes/menus/setting_menu/setting_menu.js";
 // const SettingMenu = new Setting_Menu();
